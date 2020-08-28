65f009cfef2c5d81002195ec8790ed7c1a278831
genchik1 <geno.matveev@gmail.com>
2020-08-28 19:20:14 +0400

    add host

--- jediAcademy/settings.py
+++ jediAcademy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '=fxm!656jaf%tqtmdgk(ws4sd2cdsq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '91.146.10.79']
 
 
 EMAIL_HOST = 'localhost'
