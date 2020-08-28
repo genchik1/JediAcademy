8fd6cf18f9ed7fc40e9fa05ff6603762b113e1a3
genchik1 <geno.matveev@gmail.com>
2020-08-28 21:26:22 +0400

    postgres

--- jediAcademy/settings.py
+++ jediAcademy/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'jediAcademy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'academy',
+        'USER' : 'postgres',
+        'PASSWORD' : '1Qwerty',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
