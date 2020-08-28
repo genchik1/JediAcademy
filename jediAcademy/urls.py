72ed9189a43f2f89f2a1511f80bb50831bfac31e
genchik1 <geno.matveev@gmail.com>
2020-08-28 09:10:30 +0400

    fav

--- jediAcademy/urls.py
+++ jediAcademy/urls.py
@@ -18,11 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('academy.urls')),
+    path('', include('favicon.urls')),
 ]
 
 
