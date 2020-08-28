843015abe04cbc5c96f31872200f9a35ce467391
genchik1 <geno.matveev@gmail.com>
2020-08-28 21:18:11 +0400

    heroku run

--- jediAcademy/settings.py
+++ jediAcademy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '=fxm!656jaf%tqtmdgk(ws4sd2cdsq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['floating-ocean-09459.herokuapp.com', '127.0.0.1']
 
 
 EMAIL_HOST = 'localhost'
